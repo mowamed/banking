53918f26c663fdefd444462492482e27b5f67e51
Mohamed Bakayoko <mowamedbakus@gmail.com>
2021-06-23 01:59:17 +0000

    add gorilla/mux

--- app/app.go
+++ app/app.go
@@ -1,16 +1,26 @@
 package app
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func Start() {
 
-	mux := http.NewServeMux()
+	router := mux.NewRouter()
 
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet)
+	router.HandleFunc("/customers", getAllCustomers)
 
-	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer)
+
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	customerId := vars["customer_id"]
+	fmt.Fprintf(w, customerId)
 }
